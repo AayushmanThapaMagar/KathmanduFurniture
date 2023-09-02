ef3e43405f90fef31da7921dfe0af3137f43afc3
Aayushman Thapa Magar <aayushman.magar@cryptogennepal.com>
2023-09-02 08:30:38 +0545

    changs to featured prodcuts page

--- app/components/custom/featured.jsx
+++ app/components/custom/featured.jsx
@@ -1,19 +1,32 @@
 export default function FeaturedProducts({collection}) {
-    const products = collection.collection.products.nodes;
-    return (
-      <div className='featured-products'>
-        <h1>{collection.collection.title}</h1>
-        {products.map((product)=>(
-          <a
-          key= {product.id}
-          href={`/products/${product.handle}`}
-          >
-            <img src={product.variants.nodes[0].image.url} alt={product.title}/>
-            <h2>{product.title}</h2>
-            <p>Price: {product.variants.nodes[0].price.amount} {product.variants.nodes[0].price.currencyCode}</p>
+  const products = collection.collection.products.nodes;
+  return (
+    <div className="h-auto w-auto">
+      <h1 className="align-self-center text-center">
+        {collection.collection.title}
+      </h1>
+      <div className="flex flex-wrap content-center justify-center h-auto w-auto">
+        {products.map((product) => (
+          <a key={product.id} href={`/products/${product.handle}`}>
+            <div
+              className="
+            bg-cover bg-center h-96 w-96 bg-gray-100
+            m-4 hover:scale-105 hover:shadow-lg transition duration-500 ease-in-out rounded-md"
+            >
+              <h2>{product.title}</h2>
+              <p>
+                Price: {product.variants.nodes[0].price.amount}{' '}
+                {product.variants.nodes[0].price.currencyCode}
+              </p>
+              <img
+                className="obejct-cover object-fill overflow-clip hover:scale-110 hover:shadow-xl duration-700"
+                src={product.variants.nodes[0].image.url}
+                alt={product.title}
+              />
+            </div>
           </a>
         ))}
-  
       </div>
-    )
-  }
+    </div>
+  );
+}
--- app/routes/($locale)._index.jsx
+++ app/routes/($locale)._index.jsx
@@ -2,20 +2,20 @@ import {defer} from '@shopify/remix-oxygen';
 import {Await, useLoaderData, Link} from '@remix-run/react';
 import {Suspense} from 'react';
 import {Image, Money} from '@shopify/hydrogen';
-import Hero from "~/components/custom/hero";
-import FeaturedProducts from "~/components/custom/featured";
+import Hero from '~/components/custom/hero';
+import FeaturedProducts from '~/components/custom/featured';
 
 export const meta = () => {
   return [
     {title: 'Kathmandu Furniture | Home'},
-    {description: 'Buy Premium Furniture Online'}
+    {description: 'Buy Premium Furniture Online'},
   ];
 };
 
 export async function loader({context}) {
   const {storefront} = context;
   const featuredCollection = await storefront.query(FEATURED_COLLECTION_QUERY);
-  const recommendedProducts = "test"
+  const recommendedProducts = 'test';
 
   return defer({featuredCollection, recommendedProducts});
 }
@@ -29,7 +29,6 @@ export default function Homepage() {
     </div>
   );
 }
-
 
 const FEATURED_COLLECTION_QUERY = ` #graphql
 query {
@@ -56,4 +55,4 @@ query {
     }
   }
 }
-`
+`;
